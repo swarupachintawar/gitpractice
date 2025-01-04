f1169cda18e0f7ef06280ce04a868edf26a31fd1
pswarupa <swarupa92c@gmail.com>
2025-01-04 23:58:24 +0530

    failed iframe test

--- cypress/e2e/iframe.cy.js
+++ cypress/e2e/iframe.cy.js
@@ -5,7 +5,7 @@ describe("handling iframe",()=>{
         cy.visit("/")
         cy.get("#iframe").invoke("removeAttr","target").click()
         cy.get("#frame").then((frameinfo)=>{
-            let framebody=frameinfo.contents().find("body")
+            let framebody=frameinfo.contents().find("bod")
             cy.wrap(framebody).as('inframe')
             cy.get('@inframe').find(".sub-heading").eq(1).should('have.text',"GREAT SERVICE!")
         })
